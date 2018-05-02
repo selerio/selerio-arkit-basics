b912ea0f092ccfa3c945ecd53b503adad048f5bb
Flora Tasse <floratasse@gmail.com>
2018-05-02 11:52:26 +0100

    first commit

---
+++ SelerioARKit.framework/Headers/SelerioARKit.h
@@ -0,0 +1,19 @@
+//
+//  SelerioARKit.h
+//  SelerioARKit
+//
+//  Created by Flora Tasse on 01/05/2018.
+//  Copyright © 2018 Flora Tasse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SelerioARKit.
+FOUNDATION_EXPORT double SelerioARKitVersionNumber;
+
+//! Project version string for SelerioARKit.
+FOUNDATION_EXPORT const unsigned char SelerioARKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SelerioARKit/PublicHeader.h>
+
+
--- SelerioARKitBasics/ViewController.swift
+++ SelerioARKitBasics/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+import SelerioARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate, ARSmartSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var smartSession: ARSmartSession!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +31,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.addGestureRecognizer(UITapGestureRecognizer.init(target: self, action: #selector(ViewController.handleTap(tapRecognizer:))))
+        
+        smartSession = ARSmartSession(apiKey: "APIKEY", sceneView: sceneView)
+        smartSession.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,6 +47,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        smartSession.run()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -45,6 +56,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+        
+        smartSession.pause()
     }
     
     override func didReceiveMemoryWarning() {
@@ -62,6 +75,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let smartAnchor = anchor as? ARSmartAnchor {
+            smartAnchor.loadGeometry { (geometry, error) in
+                if (error != nil) {
+                    print("Error: \(String(describing: error))")
+                } else {
+                    node.addChildNode(SCNNode(geometry: geometry))
+                }
+            }
+            let textNode = SCNNode(geometry: SCNText(string: smartAnchor.label, extrusionDepth: 0.01))
+            let textBoundingBox = textNode.boundingBox
+            textNode.pivot = SCNMatrix4MakeTranslation((textBoundingBox.max.x - textBoundingBox.min.x) / 2, 0, 0)
+            textNode.position = SCNVector3(0.0, 0.45, 0.0)
+            textNode.scale = SCNVector3(0.008/node.scale.x, 0.008/node.scale.y, 0.008/node.scale.z)
+            textNode.geometry?.firstMaterial?.lightingModel = .constant
+            node.addChildNode(textNode)
+        }
+    }
+    
+    // MARK: - ARSmartSessionDelegate
+    func session(_ session: ARSmartSession, didFailWithError error: Error) {
+        print("Smart Session Error: \(error)")
+    }
+    
+    func session(_ session: ARSmartSession, didDetectScene scene: ARPhysicalScene?) {
+        if let scene = scene {
+            print("Number of physical objects: ", scene.anchors.count)
+        } else {
+            print("Scene not detected")
+        }
+    }
+    
+    // MARK: - ARSessionDelegate
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -76,5 +122,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+    
+    // MARK: - Gestures
+    
+    @objc func handleTap(tapRecognizer: UITapGestureRecognizer) {
+        smartSession.detect()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            sceneView.scene = SCNScene(named: "art.scnassets/ship.scn")!
+            smartSession = ARSmartSession(apiKey: "APIKEY", sceneView: sceneView)
+            smartSession.run()
+        }
     }
 }
